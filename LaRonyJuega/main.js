f7fb4f7a70796343f6c82a30ab68ef9d407a3e8e
LaRony <94471556+LaRony@users.noreply.github.com>
2021-11-16 12:03:32 -0500

    Delete LaRonyJuega directory

--- LaRonyJuega/main.js
+++
@@ -1,7 +0,0 @@
-
-document.querySelector(".menu-btn").addEventListener("click",() =>{
-    document.querySelector(".nav-menu").classList.toggle("show");
-});
-
-ScrollReveal().reveal('.showcase');
-ScrollReveal().reveal('.showcase',{delay:500});
